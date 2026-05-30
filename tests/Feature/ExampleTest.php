f728785bba1e479eca4dbbb5c9cdca7cf24ec410
William Avelar <avelar.developer@gmail.com>
2026-05-30 19:00:40 -0300

    test: update ExampleTest to reflect auth-gated root route

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,7 @@
 <?php
 
-it('returns a successful response', function () {
+it('redirects root to login for unauthenticated users', function () {
     $response = $this->get('/');
 
-    $response->assertStatus(200);
+    $response->assertRedirect('/login');
 });
